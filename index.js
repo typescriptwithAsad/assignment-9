ad50d0812b19b684733501a6378906c2b1d41fd0
asad <139746796+typescriptwithAsad@users.noreply.github.com>
2023-07-20 20:21:59 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+"use strict";
+// 9.Favorite Number: Store your favorite number in a variable. Then, using that variable, 
+// create a message that reveals your favorite number. Print that message.
+const favourteNumber = 5;
+let message = `${favourteNumber}  is my favourite number.`;
+console.log(message);
---
+++ index.ts
@@ -0,0 +1,6 @@
+// 9.Favorite Number: Store your favorite number in a variable. Then, using that variable, 
+// create a message that reveals your favorite number. Print that message.
+
+const favourteNumber  : number = 5;
+let message : string = `${favourteNumber}  is my favourite number.`;
+console.log(message);
